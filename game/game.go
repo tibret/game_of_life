bc34c6b5e6ae398a4496452260283927e0e8855c
Rob Luppi <wolf189@gmail.com>
2025-04-03 22:36:48 -0400

    Moving more logic into the game's reciever functions

--- game/game.go
+++ game/game.go
@@ -65,3 +65,62 @@ func NewGame(size int) *Game {
 
 	return &Game{Cells: c, Generation: 0, Size: int32(size), Buffer: buffer, save: save}
 }
+
+func (g *Game) Advance() {
+	for i := int32(0); i < g.Size; i++ {
+		for j := int32(0); j < g.Size; j++ {
+			alive := g.Cells[i][j]
+			liveNeighbors := 0
+
+			neighbors := Neighbors(i, j, g.Size)
+			for _, n := range neighbors {
+				// print("{", n.X, ", ", n.Y, "}, ")
+				if g.Cells[n.X][n.Y] {
+					liveNeighbors++
+				}
+			}
+
+			if alive {
+				if liveNeighbors < 2 {
+					alive = false
+				} else if liveNeighbors > 3 {
+					alive = false
+				}
+			} else {
+				if liveNeighbors == 3 {
+					alive = true
+				}
+			}
+
+			g.Buffer[i][j] = alive
+		}
+	}
+
+	for i := int32(0); i < g.Size; i++ {
+		for j := int32(0); j < g.Size; j++ {
+			g.Cells[i][j] = g.Buffer[i][j]
+		}
+	}
+
+	g.Generation = g.Generation + 1
+}
+
+var Neighbor_Offsets = [8][2]int32{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
+
+type Coord struct {
+	X int32
+	Y int32
+}
+
+func Neighbors(i int32, j int32, size int32) []Coord {
+	neighbors := make([]Coord, 8)
+	for n := range 8 {
+		offsets := Neighbor_Offsets[n]
+		x := (i + size + offsets[0]) % size
+		y := (j + size + offsets[1]) % size
+
+		neighbors[n] = Coord{X: x, Y: y}
+	}
+
+	return neighbors
+}
--- game_of_life.go
+++ game_of_life.go
@@ -65,7 +65,7 @@ func main() {
 		}
 
 		if !setup || oneFrame {
-			GameLogic(g)
+			g.Advance()
 		} else if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
 			blockX, blockY := MouseToBlockCoord(blockSize, g.Size)
 			g.Cells[blockX][blockY] = !g.Cells[blockX][blockY]
@@ -82,65 +82,6 @@ func main() {
 	}
 
 	rl.CloseWindow()
-}
-
-func GameLogic(g *game.Game) {
-	for i := int32(0); i < g.Size; i++ {
-		for j := int32(0); j < g.Size; j++ {
-			alive := g.Cells[i][j]
-			liveNeighbors := 0
-
-			neighbors := Neighbors(i, j, g.Size)
-			for _, n := range neighbors {
-				// print("{", n.X, ", ", n.Y, "}, ")
-				if g.Cells[n.X][n.Y] {
-					liveNeighbors++
-				}
-			}
-
-			if alive {
-				if liveNeighbors < 2 {
-					alive = false
-				} else if liveNeighbors > 3 {
-					alive = false
-				}
-			} else {
-				if liveNeighbors == 3 {
-					alive = true
-				}
-			}
-
-			g.Buffer[i][j] = alive
-		}
-	}
-
-	for i := int32(0); i < g.Size; i++ {
-		for j := int32(0); j < g.Size; j++ {
-			g.Cells[i][j] = g.Buffer[i][j]
-		}
-	}
-
-	g.Generation = g.Generation + 1
-}
-
-var Neighbor_Offsets = [8][2]int32{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
-
-type Coord struct {
-	X int32
-	Y int32
-}
-
-func Neighbors(i int32, j int32, size int32) []Coord {
-	neighbors := make([]Coord, 8)
-	for n := range 8 {
-		offsets := Neighbor_Offsets[n]
-		x := (i + size + offsets[0]) % size
-		y := (j + size + offsets[1]) % size
-
-		neighbors[n] = Coord{X: x, Y: y}
-	}
-
-	return neighbors
 }
 
 func DrawGame(g *game.Game, blockSize int32) {
